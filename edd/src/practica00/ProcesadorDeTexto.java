ae054026129b114d60779bb9c46c0864ee474f2c
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-02-07 17:53:39 -0600

    Se crea plantilla vacia del procesador de texto

--- edd/src/practica00/ProcesadorDeTexto.java
+++ edd/src/practica00/ProcesadorDeTexto.java
@@ -1,36 +1,35 @@
 package practica00;
 
-        /**
-         *
-         * @author lgallo
-         */
-        public class ProcesadorDeTexto {
-        /** Variables y objetos de la clase **/
-        protected String ruta;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+/**
+ *
+ * @author lgallo
+ */
+public abstract class ProcesadorDeTexto {
 
-        /**
-         * Constructor
-         */
-        protected ProcesadorDeTexto(){
+    /**
+     * Variables y objetos de la clase *
+     */
+    protected String ruta;
 
-        }
-        /**
-         * 
-         * @param nombreArchivo
-         * @return 
-         */
-        public String leeTexto(String nombreArchivo){
-    return "";  
-        }
+    /**
+     *
+     * @param nombreArchivo
+     * @return
+     * @throws java.io.IOException
+     */
+    public String leeTexto(String nombreArchivo) throws IOException{
+        BufferedReader in = new BufferedReader(new FileReader(nombreArchivo));
+        return "";
+    }
 
-        /**
-         * 
-         * @param texto
-         * @param nombreArchivo
-         * @return 
-         */
-        public String creaArchivo(String[] texto, String nombreArchivo){
-
-            return "";
-        }
-    }   
+    /**
+     *
+     * @param texto
+     * @param nombreArchivo
+     * @return
+     */
+    public abstract String creaArchivo(String[] texto, String nombreArchivo);
+}
