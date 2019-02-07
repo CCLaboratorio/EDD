f8230a5a978089c0b242c8c31b38144dfe9f33c4
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-02-07 17:40:06 -0600

    Se crea plantilla del procesador

--- edd/src/practica00/ProcesadorDeTexto.java
+++ edd/src/practica00/ProcesadorDeTexto.java
@@ -1,9 +1,36 @@
 package practica00;
 
-/**
- *
- * @author lgallo
- */
-public class ProcesadorDeTexto {
-    
-}
+        /**
+         *
+         * @author lgallo
+         */
+        public class ProcesadorDeTexto {
+        /** Variables y objetos de la clase **/
+        protected String ruta;
+
+        /**
+         * Constructor
+         */
+        protected ProcesadorDeTexto(){
+
+        }
+        /**
+         * 
+         * @param nombreArchivo
+         * @return 
+         */
+        public String leeTexto(String nombreArchivo){
+    return "";  
+        }
+
+        /**
+         * 
+         * @param texto
+         * @param nombreArchivo
+         * @return 
+         */
+        public String creaArchivo(String[] texto, String nombreArchivo){
+
+            return "";
+        }
+    }   
