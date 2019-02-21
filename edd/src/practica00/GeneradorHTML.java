899c616df46ed15834b94c21466159531ccf926e
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-02-20 20:56:35 -0600

    plantillas práctica de repaso ICC

--- edd/src/practica00/GeneradorHTML.java
+++ edd/src/practica00/GeneradorHTML.java
@@ -8,7 +8,7 @@ public class GeneradorHTML extends ProcesadorDeTexto{
 
     @Override
     public String creaArchivo(String[] texto, String nombreArchivo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "";
     }
     
 }
