07b565a7c68a83f318a6e9b6ef8259c001ebfd67
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-02-26 01:25:34 -0600

    Pruebas unitarias para el factorial iterativo

--- edd/test/ejercicios/Ejercicio01Test.java
+++ edd/test/ejercicios/Ejercicio01Test.java
@@ -1,73 +1,69 @@
 package ejercicios;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Clase para probar la implementación del factorial
- * en su versión iterativa y recursiva.
+ * Clase para probar la implementación del factorial en su versión iterativa y
+ * recursiva.
+ *
  * @author lgallo
  */
 public class Ejercicio01Test {
     
-    public Ejercicio01Test() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+    private final long[] expResult = new long[6];
+
     @Before
     public void setUp() {
+        expResult[0] = 1L;
+        expResult[1] = 1L;
+        expResult[2] = 2L;
+        expResult[3] = 6L;
+        expResult[4] = 24L;
+        expResult[5] = 120L;
     }
-    
+
     @After
     public void tearDown() {
     }
-
+    
     /**
-     * Prueba del método factorialIterativo, de la clase Ejercicio01.
-     * En esta prueba hay exceso de lineas de código, se implementa así
-     * para dar énfasis a lo que se evalua en cada parte.
+     * Prueba del método factorialIterativo, de la clase Ejercicio01,
+     * para entradas mayores o iguales a cero.
+     * 
+     * @throws ejercicios.ExcepcionFactorial
+     */ 
+    @Test
+    public void testFactorial() throws ExcepcionFactorial {
+        System.out.println("Factorial para entradas mayores o iguales a cero");
+        long[] result = new long[6];
+        
+        /*  Evaluamos el factorial hasta 5  */
+        for (int i = 0; i <= 5; i++) {
+            result[i] = Ejercicio01.factorialIterativo(i);
+        }
+        assertArrayEquals(expResult, result);
+        System.out.println("\t-> Se calculó correctamente el factorial");
+    }
+    
+    /**
+     * Prueba del método factorialIterativo, de la clase Ejercicio01,
+     * para entradas negativas.
+     * 
+     * @throws ejercicios.ExcepcionFactorial
      */
     @Test
-    public void testFactorialIterativo() {
-        System.out.println("factorialIterativo");
-        long n = 0L;
-        long expResult = 1L;
-        long result = Ejercicio01.factorialIterativo(n);
-        System.out.println("Pruebas aprovadas:");
-        
-        /*  Evaluamos el factorial de cero  */
-        assertEquals(expResult, result);
-        System.out.println("Factorial de cero");
-        
-        /* 
-            Evaluamos que la implementación soporte la entrada
-            de números negativos
-        */
-        n = -10L;
-        expResult = -1L;
-        result = Ejercicio01.factorialIterativo(n);
-        assertEquals(expResult, result);
-        System.out.println("Maneja entrada de números negativos");
-        
-        /*  Evaluamos un caso conocido del factorial.  */
-        n = 4L;
-        expResult = 24L;
-        result = Ejercicio01.factorialIterativo(n);
-        assertEquals(expResult, result);
-        System.out.println("Factorial de un número entero positivo, 4");
-        
-        //fail("The test case is a prototype.");
+    public void testExcepcionFactorial() {
+        System.out.println("Factorial para entradas negativas");
+        long n = -10L;
+        try {
+            Ejercicio01.factorialIterativo(n);
+            fail("Se esperaba ExcepcionFactorial");
+        } catch (ExcepcionFactorial ex) {
+            System.out.println("\t-> Se lanzó correctamente la excepción");
+        }
     }
 
 }
