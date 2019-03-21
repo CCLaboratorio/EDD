2fa31cb4ec60de219a3bde5f171fc9888fe2fac3
Leonardo Gallo <lgallo@ciencias.unam.mx>
2019-03-21 15:43:21 -0600

    Prueba elimina elemento T

--- edd/test/practica03/ListaLigadaTest.java
+++ edd/test/practica03/ListaLigadaTest.java
@@ -6,15 +6,13 @@
 package practica03;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author fortanel
+ * @author Leonardo Gallo
  */
 public class ListaLigadaTest {
     
